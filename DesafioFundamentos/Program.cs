337af07fae4cfcbf6d889f364bb0d98ce8c5a7f0
Gustavo Oliveira <gustavo.ramosue@gmail.com>
2025-10-16 02:25:39 -0300

    Adicao das funcoes dos metodos Adicionar, Remover e Listar veiculos e refatoracao do codigo

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -3,25 +3,21 @@
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-decimal precoInicial = 0;
-decimal precoPorHora = 0;
-
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
-precoInicial = Convert.ToDecimal(Console.ReadLine());
-
-Console.WriteLine("Agora digite o preço por hora:");
-precoPorHora = Convert.ToDecimal(Console.ReadLine());
-
-// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
-Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
-
-string opcao = string.Empty;
 bool exibirMenu = true;
 
-// Realiza o loop do menu
-while (exibirMenu)
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
+                    "Digite o preço inicial:");
+decimal precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora:");
+decimal precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+Estacionamento estacionamento = new Estacionamento(precoInicial, precoPorHora);
+
+do
 {
+    // Realiza o loop do menu
     Console.Clear();
     Console.WriteLine("Digite a sua opção:");
     Console.WriteLine("1 - Cadastrar veículo");
@@ -32,15 +28,15 @@ while (exibirMenu)
     switch (Console.ReadLine())
     {
         case "1":
-            es.AdicionarVeiculo();
+            estacionamento.AdicionarVeiculo();
             break;
 
         case "2":
-            es.RemoverVeiculo();
+            estacionamento.RemoverVeiculo();
             break;
 
         case "3":
-            es.ListarVeiculos();
+            estacionamento.ListarVeiculos();
             break;
 
         case "4":
@@ -55,5 +51,6 @@ while (exibirMenu)
     Console.WriteLine("Pressione uma tecla para continuar");
     Console.ReadLine();
 }
+while (exibirMenu);
 
 Console.WriteLine("O programa se encerrou");
